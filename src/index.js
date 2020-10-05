8bac846eb6ffd839ceed6938d3ef63255d587828
Marina <6172331@mail.ru>
2020-10-05 21:57:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let simpleBracketsConfig = bracketsConfig.reduce((acc, item) => {
+        return acc.concat(item)
+    });
+
+    let arr = [];
+    for (let char of str) {
+        if (arr[arr.length - 1] && char === arr[arr.length - 1]) {
+            arr.pop()
+        } else {
+            arr.push(simpleBracketsConfig[simpleBracketsConfig.indexOf(char) + 1])
+        }
+    }
+    return arr.length === 0;
+
 }
